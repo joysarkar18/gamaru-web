3c3bfe734292e6eb26d6f01ce2c3e08f6f43e72f
Joy Sarkar <joysarkar8171@gmail.com>
2023-09-01 15:04:52 +0530

    update

--- src/Componants/downloadNow.jsx
+++ src/Componants/downloadNow.jsx
@@ -6,7 +6,7 @@ import { saveAs } from 'file-saver'
 
 const DownloadNow = () => {
     const downloadApp = () => {
-        saveAs("https://firebasestorage.googleapis.com/v0/b/gamaru-mobile-app.appspot.com/o/joysarkar.pdf?alt=media&token=3dce6cc3-b40b-45ed-a907-28dbdf9fbc60", 'gamaru' +".pdf") // Put your image url here.
+        saveAs("https://firebasestorage.googleapis.com/v0/b/gamaru-mobile-app.appspot.com/o/base%20(2).apk?alt=media&token=dd9e1a0e-b9d5-4241-92c1-92a8d936df17", 'gamaru' +".pdf") // Put your image url here.
       }
     return (
         <div onClick={downloadApp} className='cursor-pointer fixed bottom-3 right-4 z-50'>
